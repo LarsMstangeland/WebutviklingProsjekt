6b36ebe4c195d65a1528fb9aa74e925de7816cb3
Lars <lars.magne.stangeland@gmail.com>
2022-11-09 12:04:27 +0100

    pushing config files for practial reasons

---
+++ todo-client-tests-master/server/config.ts
@@ -0,0 +1,4 @@
+process.env.MYSQL_HOST = 'mysql.stud.ntnu.no';
+process.env.MYSQL_USER = 'larsmst_Prosjekt';
+process.env.MYSQL_PASSWORD = 'SVL2022';
+process.env.MYSQL_DATABASE = 'larsmst_Prosjekt_prod';
