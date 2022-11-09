6a3f14c1c94832cfe888fb6946da1be4cd5545f2
Vetle Ekern <vetleek@gmail.com>
2022-11-09 13:44:08 +0100

    added recipe-service in server

---
+++ todo-client-tests-master/server/src/recipe/recipe-service.ts
@@ -0,0 +1,44 @@
+import pool from './mysql-pool';
+import type { RowDataPacket, ResultSetHeader } from 'mysql2';
+
+export type Recipe = {
+  id: number;
+  name: string;
+  description: number;
+  region: string;
+
+};
+
+class RecipeService {
+  /**
+   * Get task with given id.
+   */
+  get(id: number) {
+    return new Promise<Recipe | undefined>((resolve, reject) => {
+      pool.query('SELECT * FROM recipes WHERE recipe_id = ?', [id], (error: any, results: RowDataPacket[]) => {
+        if (error) return reject(error);
+
+        resolve(results[0] as Recipe);
+      });
+    });
+  }
+
+  /**
+   * Get all tasks.
+   */
+  getAll() {
+    return new Promise<Recipe[]>((resolve, reject) => {
+      pool.query('SELECT * FROM recipes', (error: any, results: RowDataPacket[]) => {
+        if (error) return reject(error);
+
+        resolve(results as Recipe[]);
+      });
+    });
+  }
+
+
+
+}
+
+const recipeService = new RecipeService();
+export default recipeService;
