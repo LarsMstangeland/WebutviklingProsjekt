15240ca16350898f1074e6a421d839c2477e14ee
Lars <lars.magne.stangeland@gmail.com>
2022-11-11 11:52:32 +0100

    added preview card to widgets

--- todo-client-tests-master/client/src/index.tsx
+++ todo-client-tests-master/client/src/index.tsx
@@ -2,7 +2,7 @@ import ReactDOM from 'react-dom';
 import * as React from 'react';
 import { Component } from 'react-simplified';
 import { HashRouter, Route } from 'react-router-dom';
-import { NavBar, Card, Alert } from './widgets';
+import { NavBar, Card, Alert, PreviewCard } from './widgets';
 import { RecipeList, RecipeDetails, RecipeEdit } from './recipe/recipe-components';
 
 class Menu extends Component {
@@ -18,7 +18,18 @@ class Menu extends Component {
 
 class Home extends Component {
   render() {
-    return <Card title="Welcome">To Food Junkies</Card>;
+    return (
+      <div>
+        <Card title="Welcome">To Food Junkies</Card>
+
+          <PreviewCard title1 ="dette er en tittel1" title2 = "dette er en tittel2" image1 = "https://i.guim.co.uk/img/media/26392d05302e02f7bf4eb143bb84c8097d09144b/446_167_3683_2210/master/3683.jpg?width=1200&quality=85&auto=format&fit=max&s=a52bbe202f57ac0f5ff7f47166906403" image2 = "https://i.guim.co.uk/img/media/26392d05302e02f7bf4eb143bb84c8097d09144b/446_167_3683_2210/master/3683.jpg?width=1200&quality=85&auto=format&fit=max&s=a52bbe202f57ac0f5ff7f47166906403">
+          </PreviewCard>
+
+
+      </div>
+
+    )
+
   }
 }
 
